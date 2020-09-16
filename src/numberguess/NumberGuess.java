2186bc5aa963aa69a4acf5d1876393b6c3d29ffa
jakecarlson <jcarlson21@taboracademy.org>
2020-09-16 10:54:15 -0400

    Update NumberGuess.java

--- src/numberguess/NumberGuess.java
+++ src/numberguess/NumberGuess.java
@@ -13,37 +13,37 @@ public class NumberGuess {
      * @throws java.lang.InterruptedException
      */
     public static void main(String[] args) throws java.io.IOException, InterruptedException{
-        
+        //Hi, jake was here
         java.io.BufferedReader keyIn = new java.io.
                 BufferedReader(new java.io.InputStreamReader(System.in));
-        
+
         int number = 45;
         int nguess = 10;
         int guess;
-        
-        System.out.println("Let's play guess the number");        
-        
-        for(int i = 1; i <= nguess ; i++){  
+
+        System.out.println("Let's play guess the number");
+
+        for(int i = 1; i <= nguess ; i++){
             System.out.print("\nGuess a number from 1 - 100: ");
-            guess = Integer.parseInt(keyIn.readLine()); 
-            
+            guess = Integer.parseInt(keyIn.readLine());
+
             if (guess == number){
                 System.out.println("Congrats, you guessed it!");
                 i = nguess;
-            }            
+            }
             if (guess > number){
                 System.out.println("Your guess is too high");
-                
+
             }
             if (guess < number){
                 System.out.println("Your guess is too low");
             }
-            
-        }  
-        
+
+        }
+
         System.out.println("GAME OVER");
-        
-        
+
+
     }
-    
+
 }
